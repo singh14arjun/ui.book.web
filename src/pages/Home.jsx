f842bd4b7ff3a11f5bf57c353b10025ff828c2be
Arjun Singh <arjun2314singh@gmail.com>
2025-05-06 15:48:15 +0530

    Add Css file for SingUp Page and done some changes in SingIn CSS page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,14 @@ function Home() {
     <div>
         <div className="row1 flex justify-around gap-2.5 my-10">
           <div className="content">
-              <h1 className='' style={{fontSize:"40px"}}>Dive into endless stories. <span className='text-fuchsia-600'> Explore now!</span></h1>
+              <h1 className='' style={{fontSize:"40px",color:"blue"}}>Dive into endless stories. <span className='text-fuchsia-600'> Explore now!</span></h1>
               <p>Expand your horizons through the power of literature.</p>
               <button>Start Now</button>
           </div>
           <div className="image">
             <img src="src\assets\photo\Image.png" alt="loading" style={{maxWidth:"400px",maxHeight:"400px"}}/>
+            <div className="circle1"></div>
+            <div className="circle2"></div>
           </div>
         </div>
     </div>
